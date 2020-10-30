e5d03f8a8542237bc9067cfbe2f5e635a881c17d
unknown <bastvdn@gmail.com>
2020-10-30 14:36:59 +0100

    exo 30/10

--- exercises/chess/model/board/board.go
+++ exercises/chess/model/board/board.go
@@ -15,18 +15,45 @@ func (c *Classic) String() string {
 // PieceAt retrievves piece at give coordinates.
 // Returns nil if no piece was found.
 func (c *Classic) PieceAt(at coord.ChessCoordinates) piece.Piece {
-	panic("not implemented") // TODO: Implement
+	panic("not implemented") 
+	x, _ := at.Coord(0)
+	y, _ := at.Coord(1)
+	pos := c[x][y]
+	if val != nil {
+		return val
+	}
+	return nil
+	
 }
 
 // MovePiece moves a piece from given coordinates to
 // given coordinates.
 // Returns an error if destination was occupied.
 func (c *Classic) MovePiece(from coord.ChessCoordinates, to coord.ChessCoordinates) error {
-	panic("not implemented") // TODO: Implement
+	// TODO: Implement
+	if c.PieceAt(from) != nil {
+		x, _ := from.Coord(0)
+		y, _ := from.Coord(1)
+		pos := c[x][y]
+		return c.PlacePieceAt(pos, to)
+	}
+	else{
+		return fmt.Errorf("No piece at %v", from.String())
+	}
+
+
 }
 
 // PlacePieceAt places a given piece at given location.
 // Returns an error if destination was occupied.
 func (c *Classic) PlacePieceAt(p piece.Piece, at coord.ChessCoordinates) error {
-	panic("not implemented") // TODO: Implement
+	if c.PieceAt(at) != nil {
+		return fmt.Errorf("Piece at %v", at.String())
+	}	
+	else{
+		x, _ := at.Coord(0)
+		y, _ := at.Coord(1)
+		c[x][y] = pos
+		return nil
+	}
 }
